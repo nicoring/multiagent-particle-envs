4c0a40af46320d45067d81189d720c670fa19f73
Nico Ring <ringnico@web.de>
2018-06-16 15:12:37 +0100

    fix use of gym's MultiDiscrete space

--- multiagent/environment.py
+++ multiagent/environment.py
@@ -57,7 +57,7 @@ class MultiAgentEnv(gym.Env):
             if len(total_action_space) > 1:
                 # all action spaces are discrete, so simplify to MultiDiscrete action space
                 if all([isinstance(act_space, spaces.Discrete) for act_space in total_action_space]):
-                    act_space = spaces.MultiDiscrete([[0,act_space.n-1] for act_space in total_action_space])
+                    act_space = spaces.MultiDiscrete([act_space.n for act_space in total_action_space])
                 else:
                     act_space = spaces.Tuple(total_action_space)
                 self.action_space.append(act_space)
