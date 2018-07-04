7c9a8986ed4c90c9262e00a8acdebf06cce48554
Nico Ring <ringnico@web.de>
2018-07-04 21:50:31 +0100

    simple spread env

--- multiagent/scenarios/simple_spread.py
+++ multiagent/scenarios/simple_spread.py
@@ -1,3 +1,5 @@
+import itertools as it
+
 import numpy as np
 from multiagent.core import World, Agent, Landmark
 from multiagent.scenario import BaseScenario
@@ -49,7 +51,7 @@ class Scenario(BaseScenario):
         occupied_landmarks = 0
         min_dists = 0
         for l in world.landmarks:
-            dists = [np.sqrt(np.sum(np.square(a.state.p_pos - l.state.p_pos))) for a in world.agents]
+            dists = [self.dist(a, l) for a in world.agents]
             min_dists += min(dists)
             rew -= min(dists)
             if min(dists) < 0.1:
@@ -61,18 +63,11 @@ class Scenario(BaseScenario):
                     collisions += 1
         return (rew, collisions, min_dists, occupied_landmarks)
 
-
-    def is_collision(self, agent1, agent2):
-        delta_pos = agent1.state.p_pos - agent2.state.p_pos
-        dist = np.sqrt(np.sum(np.square(delta_pos)))
-        dist_min = agent1.size + agent2.size
-        return True if dist < dist_min else False
-
     def reward(self, agent, world):
         # Agents are rewarded based on minimum agent distance to each landmark, penalized for collisions
         rew = 0
         for l in world.landmarks:
-            dists = [np.sqrt(np.sum(np.square(a.state.p_pos - l.state.p_pos))) for a in world.agents]
+            dists = [self.dist(a, l) for a in world.agents]
             rew -= min(dists)
         if agent.collide:
             for a in world.agents:
@@ -80,8 +75,12 @@ class Scenario(BaseScenario):
                     rew -= 1
         return rew
 
+    def done(self, agent, world):
+        cover = all(any(self.is_collision(a, l) for a in world.agents) for l in world.landmarks)
+        return cover
+
     def observation(self, agent, world):
-        # get positions of all entities in this agent's reference frame
+        # get positions of all landmarks in this agent's reference frame
         entity_pos = []
         for entity in world.landmarks:  # world.entities:
             entity_pos.append(entity.state.p_pos - agent.state.p_pos)
@@ -94,6 +93,7 @@ class Scenario(BaseScenario):
         other_pos = []
         for other in world.agents:
             if other is agent: continue
-            comm.append(other.state.c)
+            if not other.silent:
+                comm.append(other.state.c)
             other_pos.append(other.state.p_pos - agent.state.p_pos)
-        return np.concatenate([agent.state.p_vel] + [agent.state.p_pos] + entity_pos + other_pos + comm)
+        return np.concatenate([agent.state.p_vel] + entity_pos + other_pos + comm)
