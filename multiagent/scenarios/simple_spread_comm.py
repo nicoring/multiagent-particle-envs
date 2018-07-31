ed2f02335f24eb7e23e8672528bf4bee67937438
Nico Ring <ringnico@web.de>
2018-07-31 23:32:17 +0100

    removes others position for comm spread env

--- multiagent/scenarios/simple_spread_comm.py
+++ multiagent/scenarios/simple_spread_comm.py
@@ -10,3 +10,22 @@ class Scenario(NoCommScenario):
         for agent in world.agents:
             agent.silent = False
         return world
+
+    def observation(self, agent, world):
+        # get positions of all landmarks in this agent's reference frame
+        entity_pos = []
+        for entity in world.landmarks:  # world.entities:
+            entity_pos.append(entity.state.p_pos - agent.state.p_pos)
+        # entity colors
+        entity_colors = []
+        for entity in world.landmarks:  # world.entities:
+            entity_colors.append(entity.color)
+        # communication of all other agents
+        comm = []
+        other_pos = []
+        for other in world.agents:
+            if other is agent: continue
+            if not other.silent:
+                comm.append(other.state.c)
+            other_pos.append(other.state.p_pos - agent.state.p_pos)
+        return np.concatenate([agent.state.p_vel] + entity_pos + entity_colors + comm)
